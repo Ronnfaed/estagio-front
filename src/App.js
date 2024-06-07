79512754d5283a441a709127e5ccca2b19a6a4de
Ronnfaed <prinzantiochus@gmail.com>
2024-06-07 17:12:23 -0300

    PDF

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import axios from "axios";
 import './App.css';
 
+
 function App() {
+
+  const[title, setTitle] = useState("");
+  const[file, setFile] = useState("");
+  const [allImage, setAllImage] = useState(null);
+
+  const getPdf = async () => {
+    const result = await axios.get("http://localhost:5000/get-files");
+    console.log(result.data.data);
+    setAllImage(result.data.data);
+  };
+
+  useEffect(() => {
+
+    getPdf();
+
+  }, []);
+
+
+  const submitImage = async(e) => {
+    e.preventDefault(); 
+    const formData = new FormData(); 
+    formData.append("title", title); 
+    formData.append("file", file); 
+    console.log(title, file);
+    const result = await axios.post(
+      "http://localhost:5000/upload-files", 
+      formData, {
+      header: {"Content-Type": "multipart/form-data"},
+      }
+    );
+  
+    console.log(result);
+
+    if(result.data.status === "ok"){
+      alert("PDF subido com sucesso");
+      getPdf();
+    }
+  
+  };
+
+  const showPdf=(pdf)=>{
+    window.open(`http://localhost:5000/files/${pdf}`, "_blank", "noreferrer");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+
+      <form className="formUpload" onSubmit={submitImage}>
+
+        <h4>Leitor de PDF</h4>
+        <br></br>
+        <input type="text" className="formInputs" placeholder="Título" required onChange={(e)=>setTitle(e.target.value)}/>
+        <br></br>
+        <input type="file" className="formInputs" accept="application/pdf" required onChange={(e)=>setFile(e.target.files[0])}/>
+        <br></br>
+        <button class="btn" type="submit">
+        Enviar
+        </button>
+
+      </form>
+
+      <div className = "saida">
+
+        <h4>Saída:</h4>
+
+        <div className="output-div">
+          {allImage == null
+          ?""
+          : allImage.map((data)=>{
+            return (
+              <div className="output-inner-div">
+              <h6>Título: {data.title}</h6>
+              <button className="btn btn-primary" onClick={()=>showPdf(data.pdf)}>Mostrar</button>
+              </div>
+            );
+          })}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
